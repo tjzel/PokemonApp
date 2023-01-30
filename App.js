fc80b503cb5308e91d3ec1c04f1565d9db8f3245
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-01-30 12:02:43 +0100

    init

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+  .then((response) => response.json())
+  .then((json) => console.log("xx"));
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>

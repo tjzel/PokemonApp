8559eeba42f298c5bc0109c7424149a854f29f55
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-01-31 11:26:43 +0100

    able to favourite a pokemon!

--- App.js
+++ App.js
@@ -1,74 +1,91 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import FavouritePokemonTab from './components/FavouritePokemonTab';
-import PokemonListTab from './components/PokemonListTab';
-import PokemonMapTab from './components/PokemonMapTap';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import * as React from "react";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import FavouritePokemonTab from "./components/FavouritePokemonTab";
+import PokemonListTab from "./components/PokemonListTab";
+import PokemonMapTab from "./components/PokemonMapTap";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 export default function App() {
-
   const Tab = createBottomTabNavigator();
-  const pokemonListIcon = require('./assets/pokemon-list-icon.png');
-  const [favouritePokemon, setFavouritePokemon] = React.useState(null); //how to pass it?
+  const pokemonListIcon = require("./assets/pokemon-list-icon.png");
+  const [favouritePokemon, setFavouritePokemon] = React.useState("kakuna");
 
   return (
-      <NavigationContainer>
-        <Tab.Navigator initialRouteName="Favourite Pokemon">
-          <Tab.Screen
-            name="Favourite Pokemon"
-            component={FavouritePokemonTab}
-            options={{
-              tabBarIcon: () => {
-                return (
-                  <Image
-                    style={[styles.icon]}
-                    source={{uri:'https://cdn-icons-png.flaticon.com/512/287/287221.png'}}
-                  />
-                )
-              }
-            }}
-          />
-          <Tab.Screen 
-            name="Pokemon List" 
-            component={PokemonListTab}
-            options={{
-              tabBarIcon: () => {
-                return (
-                  <Image
-                    style={[styles.icon, {width: 48}]}
-                    source={pokemonListIcon}
-                  />
-                )
-              }
-            }}
-          />
-          <Tab.Screen 
-            name="Pokemon Map" 
-            component={PokemonMapTab}
-            options={{
-              tabBarIcon: () => {
-                return (
-                  <Image
-                    style={[styles.icon]}
-                    source={{uri:'https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-2/135/_Pokemon_Location-512.png'}}
-                  />
-                )
-              }
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Favourite Pokemon">
+        <Tab.Screen
+          name="Favourite Pokemon"
+          options={{
+            tabBarIcon: () => {
+              return (
+                <Image
+                  style={[styles.icon]}
+                  source={{
+                    uri: "https://cdn-icons-png.flaticon.com/512/287/287221.png",
+                  }}
+                />
+              );
+            },
+          }}
+        >
+          {(props) => (
+            <FavouritePokemonTab
+              favouritePokemon={favouritePokemon}
+              setFavouritePokemon={setFavouritePokemon}
+              {...props}
+            />
+          )}
+        </Tab.Screen>
+        <Tab.Screen
+          name="Pokemon List"
+          options={{
+            tabBarIcon: () => {
+              return (
+                <Image
+                  style={[styles.icon, { width: 48 }]}
+                  source={pokemonListIcon}
+                />
+              );
+            },
+          }}
+        >
+          {(props) => (
+            <PokemonListTab
+              favouritePokemon={favouritePokemon}
+              setFavouritePokemon={setFavouritePokemon}
+              {...props}
+            />
+          )}
+        </Tab.Screen>
+        <Tab.Screen
+          name="Pokemon Map"
+          component={PokemonMapTab}
+          options={{
+            tabBarIcon: () => {
+              return (
+                <Image
+                  style={[styles.icon]}
+                  source={{
+                    uri: "https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-2/135/_Pokemon_Location-512.png",
+                  }}
+                />
+              );
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   image: {
     maxHeight: 320,
@@ -79,22 +96,22 @@ const styles = StyleSheet.create({
   },
   someScreen: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
   button: {
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
   buttonContainer: {
     height: 20,
     width: 120,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
-  icon:{
+  icon: {
     height: 28,
     width: 28,
   },

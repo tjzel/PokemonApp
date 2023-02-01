17a428f2d06832616f8ca3ff3c7eb12be6156275
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-02-01 12:14:44 +0100

    migrated to tsx

--- components/PokemonMapTap.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
-
-export default function PokemonMapTab(){
-  return(
-    <View style={{width:200, heighy:200}}>
-      <Text>Pokemon Map tab is empty...</Text>
-    </View>
-  )
-}
